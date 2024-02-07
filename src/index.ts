5021186a4af5d2787875a8837247ec6adaae23a2
Eli Wood <mreliwood1@gmail.com>
2024-02-07 11:16:10 -0800

    revising console log

--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,38 @@ import { watch } from 'fs';
 import { exec } from 'child_process';
 
 const watchDirectory = (directory: string = './') => {
-	console.log(' ');
+	console.log('');
 	console.log('🏗️  autobuilder is watching this directory for changes');
-	console.log(' ');
+	console.log('');
 
 	const ac = new AbortController();
 
 	const options = {
-		persistent: true,
 		recursive: true,
 		signal: ac.signal
 	};
+
+	let logged = false;
 
 	watch(directory, options, (eventType, filename) => {
 		if (filename?.includes('node_modules')) return;
 		if (filename?.includes('dist')) return;
 		if (filename?.includes('lib')) return;
 
-		console.log(' ');
-		console.log('autobuilder detected changes to ' + filename);
-		console.log(' ');
-		console.log('  🏗️  building...');
-		console.log(' ');
+		if (logged) {
+			logged = false;
+			return;
+		}
 
-		ac.abort();
-		exec('npm run build');
-		watchDirectory(directory);
+		console.log('');
+		console.log('autobuilder detected changes to ' + filename);
+		console.log('');
+		console.log('  🏗️  building...');
+		console.log('');
+
+		logged = true;
+
+		exec('npm run build', (error, stdout) => console.log(stdout));
 	});
 };
 
